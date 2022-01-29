1fed8bdcb63b8ec176c2d011e238098875623980
AndreiShev <shevelyov.andreas@gmail.com>
2022-01-29 15:26:17 +0600

    Работа с DOM. Загрузка документа. Первый пулреквест

--- 04-oop-basic-intro-to-dom/1-column-chart/index.js
+++ 04-oop-basic-intro-to-dom/1-column-chart/index.js
@@ -3,78 +3,78 @@ export default class ColumnChart {
     chartHeight = 50;
 
     constructor(obj = {data: [], label: "", value: "", link: "", formatHeading: ()=>{}}) {
-        this.data = obj.data || [];
-        this.label = obj.label || "";
-        this.value = obj.value || "";
-        this.link = obj.link || "";
-        this.formatHeading = obj.formatHeading;
+      this.data = obj.data || [];
+      this.label = obj.label || "";
+      this.value = obj.value || "";
+      this.link = obj.link || "";
+      this.formatHeading = obj.formatHeading;
 
-        this.element = this.render();
+      this.element = this.render();
     }
 
-    get template(){
-        return `
-        <div class="column-chart" style="--chart-height: ${this.chartHeight}">
-            <div class="column-chart__title">
-                Total ${this.label}
-                <a href='${this.link}' class="column-chart__link">View all</a>
-            </div>
-            <div class="column-chart__container">
-                <div data-element="header" class="column-chart__header">${this.getValue()}</div>
-                <div data-element="body" class="column-chart__chart">
-                    ${this.getChartChildren()}
-                </div>
-            </div>
+    template() {
+      return `
+      <div class="column-chart" style="--chart-height: ${this.chartHeight}">
+        <div class="column-chart__title">
+          Total ${this.label}
+          <a href='${this.link}' class="column-chart__link">View all</a>
         </div>
-        `
+        <div class="column-chart__container">
+          <div data-element="header" class="column-chart__header">${this.getValue()}</div>
+          <div data-element="body" class="column-chart__chart">
+              ${this.getChartChildren()}
+          </div>
+        </div>
+      </div>
+      `;
     }
 
     render() {
-        const template = document.createElement('div');
+      const template = document.createElement('div');
 
-        template.innerHTML = this.template;
-        this.element = template.firstElementChild;
+      template.innerHTML = this.template();
+      this.element = template.firstElementChild;
 
-        if(this.data.length === 0){
-            this.element.classList.add('column-chart_loading');
+      if (this.data.length === 0) {
+        this.element.classList.add('column-chart_loading');
+      }
+
+      return this.element;
+    }
+
+    getChartChildren() {
+      let result = "";
+      if (this.data.length > 0) {
+        for( let i = 0; i < this.data.length; i++) {
+          const maxValue = Math.max(...this.data);
+          const scale = this.chartHeight / maxValue;
+
+          result += `<div style='--value: ${String(Math.floor(this.data[i] * scale))}' data-tooltip='${(this.data[i] / maxValue * 100).toFixed(0)}%'></div> 
+          `;
         }
+      }
 
-        return this.element;
+      return result;
     }
 
-    getChartChildren(){
-        let result = "";
-        if(this.data.length > 0){
-            for( let i = 0; i < this.data.length; i++){
-                const maxValue = Math.max(...this.data);
-                const scale = 50 / maxValue;
+    getValue() {
+      if (this.formatHeading) {
+        return this.formatHeading(this.value);
+      }
 
-                result += `<div style='--value: ${String(Math.floor(this.data[i] * scale))}' data-tooltip='${(this.data[i] / maxValue * 100).toFixed(0)}%'></div> 
-`;
-            }
-        }
-
-        return result;
+      return this.value;
     }
 
-    getValue(){
-        if(this.formatHeading){
-            return this.formatHeading(this.value);
-        }
-
-        return this.value;
+    update(obj) {
+      this.data = obj.data || this.data;
+      this.render;
     }
 
-    update(obj){
-        this.data = obj.data || this.data;
-        this.render;
-    }
-
-    remove () {
-        this.element.remove();
+    remove() {
+      this.element.remove();
     }
     
     destroy() {
-        this.remove();
+      this.remove();
     }
 }
--- 05-dom-document-loading/1-notification/index.js
+++ 05-dom-document-loading/1-notification/index.js
@@ -1,3 +1,61 @@
 export default class NotificationMessage {
+  static activeNotification;
 
+  element;
+  timerId;
+  
+  constructor(message, obj = {duration: 0, type: 'success'}) {
+    this.message = message;
+    this.durationSecond = (this.duration/1000) + 's';
+    this.duration = obj.duration;
+    this.type = obj.type;
+    
+    this.render();
+  }
+
+  template() {
+    return `<div class="notification ${this.type}" style="--value:${this.duration}">
+      <div class="timer"></div>
+      <div class="inner-wrapper">
+      <div class="notification-header">Notification</div>
+        <div class="notification-body">${this.message}</div>
+      </div>
+    </div>`;
+  }
+  
+  render() {
+    const tempElement = document.createElement('div');
+
+    tempElement.innerHTML = this.template();
+
+    this.element = tempElement.firstChild;
+  }
+
+  show(parent = document.body) {
+    if (NotificationMessage.activeNotification) {
+      NotificationMessage.activeNotification.remove();
+    }
+
+    parent.append(this.element);
+
+    this.timerId = setTimeout(() => {
+      this.remove();
+    }, this.duration);
+
+    NotificationMessage.activeNotification = this;
+  }
+
+  remove() {
+    clearTimeout(this.timerId);
+
+    if (this.element) {
+      this.element.remove();
+    }
+  }
+
+  destroy() {
+    this.remove();
+    this.element = null;
+    NotificationMessage.activeNotification = null;
+  }
 }
