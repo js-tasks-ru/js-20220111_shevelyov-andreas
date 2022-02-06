473e30c899858782edc2afb027055f0a76f1f7c3
AndreiShev <shevelyov.andreas@gmail.com>
2022-02-07 02:50:46 +0600

    События в браузере правктика. Fetch API

--- 06-events-practice/1-sortable-table-v2/index.js
+++ 06-events-practice/1-sortable-table-v2/index.js
@@ -1,8 +1,157 @@
 export default class SortableTable {
+  element;
+  subElements = {};
+
   constructor(headersConfig, {
     data = [],
     sorted = {}
   } = {}) {
+    this.headersConfig = headersConfig;
+    this.data = Array.isArray(data) ? data : data.data;
+    this.sorted = sorted;
 
+    this.render();
+    this.sort(sorted.id, sorted.order);
+  }
+
+  getTableHeader() {
+    return `<div data-element="header" class="sortable-table__header sortable-table__row">
+      ${this.headersConfig.map(item => this.getHeaderRow(item)).join('')}
+    </div>`;
+  }
+
+  getHeaderRow({id, title, sortable}) {
+    const order = this.sorted.id === id ? this.sorted.order : 'asc';
+
+    return `
+      <div class="sortable-table__cell" data-id="${id}" data-sortable="${sortable}" data-order="${order}">
+        <span>${title}</span>
+        <span data-element="arrow" class="sortable-table__sort-arrow">
+          <span class="sort-arrow"></span>
+        </span>
+      </div>
+    `;
+  }
+
+  getTableBody() {
+    return `
+      <div data-element="body" class="sortable-table__body">
+        ${this.getTableRows(this.data)}
+      </div>`;
+  }
+
+  getTableRows(data) {
+    return data.map(item => {
+      return `
+        <a href="/products/${item.id}" class="sortable-table__row">
+          ${this.getTableRow(item)}
+        </a>`;
+    }).join('');
+  }
+
+  getTableRow(item) {
+    const cells = this.headersConfig.map(({id, template}) => {
+      return {
+        id,
+        template
+      };
+    });
+
+    return cells.map(({id, template}) => {
+      return template
+        ? template(item[id])
+        : `<div class="sortable-table__cell">${item[id]}</div>`;
+    }).join('');
+  }
+
+  getTable() {
+    return `
+      <div class="sortable-table">
+        ${this.getTableHeader()}
+        ${this.getTableBody()}
+      </div>`;
+  }
+
+  render() {
+    const wrapper = document.createElement('div');
+
+    wrapper.innerHTML = this.getTable();
+
+    const element = wrapper.firstElementChild;
+
+    this.element = element;
+    this.subElements = this.getSubElements(element);
+    this.setEventListeners();
+  }
+
+  eventClick = event => {
+    const column = event.target.closest('[data-sortable="true"]');
+    const field = column.dataset.id;
+ 
+    if (column) {
+      const newOrder = column.dataset.order === "asc" ? "desc" : "asc";
+      column.dataset.order = newOrder;
+
+      this.sort(field, newOrder);
+    }
+  };
+
+  setEventListeners() { 
+    this.subElements.header.addEventListener('pointerdown', this.eventClick);
+  }
+
+  removeEventListeners() {
+    this.subElements.header.removeEventListener('pointerdown', this.eventClick);
+  }
+
+  sort(field, order) {
+    const sortedData = this.sortData(field, order);
+
+    this.subElements.body.innerHTML = this.getTableRows(sortedData);
+  }
+
+  sortData(field, order) {
+    const arr = [...this.data];
+    const column = this.headersConfig.find(item => item.id === field);
+    const { sortType } = column;
+    const directions = {
+      asc: 1,
+      desc: -1
+    };
+    const direction = directions[order];
+
+    return arr.sort((a, b) => {
+      switch (sortType) {
+      case 'number':
+        return direction * (a[field] - b[field]);
+      case 'string':
+        return direction * a[field].localeCompare(b[field], ['ru']);
+      default:
+        return direction * (a[field] - b[field]);
+      }
+    });
+  }
+
+  getSubElements(element) {
+    const elements = element.querySelectorAll('[data-element]');
+
+    return [...elements].reduce((accum, subElemenet) => {
+      accum[subElemenet.dataset.element] = subElemenet;
+
+      return accum;
+    }, {});
+  }
+
+
+  remove () {
+    if (this.element) {
+      this.element.remove();
+    }
+  }
+
+  destroy() {
+    this.remove();
+    this.element = null;
+    this.subElements = {};
   }
 }
