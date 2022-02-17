cf2c559aab687306aa266a1ef7ec67b386a7b2d4
AndreiShev <shevelyov.andreas@gmail.com>
2022-02-17 19:11:24 +0600

    Роутинг в веб приложениях

--- 10-routes-browser-history-api/1-dashboard-page/index.js
+++ 10-routes-browser-history-api/1-dashboard-page/index.js
@@ -8,5 +8,154 @@ import fetchJson from './utils/fetch-json.js';
 const BACKEND_URL = 'https://course-js.javascript.ru/';
 
 export default class Page {
+  element;
+  subElements = {};
+  components = {};
+  url = new URL('api/dashboard/bestsellers', BACKEND_URL);
 
+  initComponents() {
+    const now = new Date();
+    const to = new Date();
+    const from = new Date(now.setMonth(now.getMonth() - 1));
+
+    const rangePicker = new RangePicker({
+      from,
+      to
+    });
+
+    const sortableTable = new SortableTable(header, {
+      url: 'api/dashboard/bestsellers',
+      isSortLocally: true
+    });
+
+    const ordersChart = new ColumnChart({
+      url: 'api/dashboard/orders',
+      range: {
+        from,
+        to
+      },
+      label: 'orders',
+      link: '#'
+    });
+
+    const salesChart = new ColumnChart({
+      url: 'api/dashboard/sales',
+      range: {
+        from,
+        to
+      },
+      label: 'sales'
+    });
+
+    const customersChart = new ColumnChart({
+      url: 'api/dashboard/customers',
+      range: {
+        from,
+        to
+      },
+      label: 'customers',
+    });
+
+    this.components = {
+      sortableTable,
+      ordersChart,
+      salesChart,
+      customersChart,
+      rangePicker
+    };
+  }
+
+  renderComponents() {
+    Object.keys(this.components).forEach(component => {
+      const root = this.subElements[component];
+      const { element } = this.components[component];
+
+      root.append(element);
+    });
+  }
+
+  loadData(from, to) {
+    this.url.searchParams.set('_start', '1');
+    this.url.searchParams.set('_end', '21');
+    this.url.searchParams.set('_sort', 'title');
+    this.url.searchParams.set('_order', 'asc');
+    this.url.searchParams.set('from', from.toISOString());
+    this.url.searchParams.set('to', to.toISOString());
+
+    return fetchJson(this.url);
+  }
+
+  async updateComponents(from, to) {
+    const data = await this.loadData(from, to);
+
+    this.components.sortableTable.updata(data);
+    this.components.ordersChart.updata(from, to);
+    this.components.salesChart.updata(from, to);
+    this.components.customersChart.updata(from, to);
+  }
+
+  initEventListeners() {
+    this.components.rangePicker.element.addEventListener('data-select', event => {
+      const { from, to } = event.detail;
+
+      this.updateComponents(from, to);
+    });
+  }
+
+  getTemplate() {
+    return `<div class="dashboard">
+      <div class="content__top-panel">
+        <h2 class="page-title">Dashboard</h2>
+        <div data-element="rangePicker"></div>
+      </div>
+      <div data-element="chartsRoot" class="dashboard__charts">
+        <div data-element="ordersChart" class="dashboard__chart_orders"></div>
+        <div data-element="salesChart" class="dashboard__chart_sales"></div>
+        <div data-element="customersChart" class="dashboard__chart_customers"></div>
+      </div>
+
+      <h3 class="block-title">Best sellers</h3>
+
+      <div data-element="sortableTable"></div>
+    </div>`;
+  }
+
+  render() {
+    const element = document.createElement('div');
+
+    element.innerHTML = this.getTemplate();
+
+    this.element = element.firstElementChild;
+    this.subElements = this.getSubElements(this.element);
+
+    this.initComponents();
+    this.renderComponents();
+    this.initEventListeners();
+
+    return this.element;
+  }
+
+  getSubElements(element) {
+    const subElements = {};
+    const elements = element.querySelectorAll('[data-element]');
+
+    for (const item of elements) {
+      subElements[item.dataset.element] = item;
+    }
+
+    return subElements;
+  }
+
+  remove() {
+    this.element.remove();
+  }
+
+  destroy() {
+    this.remove();
+
+    for (const component of Object.values(this.components)) {
+      component.destroy();
+    }
+  }
 }
+ 
